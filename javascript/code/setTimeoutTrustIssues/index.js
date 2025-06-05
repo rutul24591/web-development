d78b4d3eed81ed423afb05eaaf77a1e340bdfba9
Rutul Amin <rutulamin.ra@gmail.com>
2025-06-05 14:06:37 +0530

    setTimeout Trust

---
+++ javascript/code/setTimeoutTrustIssues/index.js
@@ -0,0 +1,19 @@
+console.log("Start of the script");
+
+setTimeout(function(){
+    console.log("This is a callback statement");
+}, 5000);
+
+console.log("End of the script");
+
+// This is a blocking code. Main thread is blocked for 10 seconds.
+// Mimicking a million lines of code which takes 10 seconds to execute and Global execution context is blocked for 10 seconds.
+let startDate = new Date().getTime();
+let endDate = startDate;
+
+while(endDate < startDate + 10000){
+    endDate = new Date().getTime();
+}
+console.log("While completed");
+
+
