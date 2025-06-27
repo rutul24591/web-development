6a7ca65b7b8bf2d4dc1415f4b0582a69f64e6e57
Rutul Amin <rutulamin.ra@gmail.com>
2025-06-27 09:44:24 +0530

    Insert in SLL

--- javascript/data-structure-in-js/SinglyLinkedList/Node.js
+++ javascript/data-structure-in-js/SinglyLinkedList/Node.js
@@ -1,5 +1,5 @@
 class Node {
-    constructor(value){
+    constructor(value) {
         // Specifically we are saying that this is the value for this node.
         this.value = value;
         this.next = null;
