22b5cf8ef2572f4ede3b31698abf116662358ddc
Rutul Amin <rutulamin.ra@gmail.com>
2025-07-31 14:27:50 +0530

    Adding block scope and shadowing examples

---
+++ javascript/code/block-scope-and-shadowing/example1.js
@@ -0,0 +1,10 @@
+// Block
+if (true) {
+    let a = 10;
+    console.log(a);
+}
+
+/** Block is nothing but a group of statements wrapped around {} and placed
+ * where javascript expects a single statement. Blocks are used to group code together,
+ * especially in control structure like if, while, for or in function declarations.
+ */
