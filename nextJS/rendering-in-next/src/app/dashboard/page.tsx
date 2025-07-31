3547decc26220bbb9823ace6834549fa67563ef3
Rutul Amin <rutulamin.ra@gmail.com>
2025-07-31 13:51:13 +0530

    rendering in next js

--- nextJS/rendering-in-next/src/app/dashboard/page.tsx
+++ nextJS/rendering-in-next/src/app/dashboard/page.tsx
@@ -1,17 +1,22 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
+import { useState } from "react";
 
 const AboutPage = () => {
-	const [name, setName] = useState('');
-	console.log(`Dashboard client component`);
-	return (
-		<div>
-			<h1>Dashboard page</h1>
-			<input value={name} onChange={(event) => setName(event.target.value)} />
-			<p>Hello, {name}!</p>
-		</div>
-	);
+    const [name, setName] = useState("");
+    // Unlike logs in server component where a `Server` tag is displayed before a log,
+    // below log won't have any tag before comment(No tag in client component).
+    console.log(`Dashboard client component`);
+    return (
+        <div>
+            <h1>Dashboard page</h1>
+            <input
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+            />
+            <p>Hello, {name}!</p>
+        </div>
+    );
 };
 
 export default AboutPage;
--- nextJS/rendering-in-next/src/app/product-reviews/page.tsx
+++ nextJS/rendering-in-next/src/app/product-reviews/page.tsx
@@ -1,20 +1,22 @@
-import { Suspense } from 'react';
+import { Suspense } from "react";
 
-import Product from '@/components/product';
-import Reviews from '@/components/reviews';
+import Product from "@/components/product";
+import Reviews from "@/components/reviews";
 
+/** Without Suspense it will load everything first and then only render
+ * ie after 6sec (6000 is set for promise resolve in Reviews). Try commenting Suspense below */
 const ProductReviews = () => {
-	return (
-		<div>
-			<h1>Product reviews</h1>
-			<Suspense fallback={<p>Loading product details..</p>}>
-				<Product />
-			</Suspense>
-			<Suspense fallback={<p>Loading reviews details..</p>}>
-				<Reviews />
-			</Suspense>
-		</div>
-	);
+    return (
+        <div>
+            <h1>Product reviews</h1>
+            <Suspense fallback={<p>Loading product details..</p>}>
+                <Product />
+            </Suspense>
+            <Suspense fallback={<p>Loading reviews details..</p>}>
+                <Reviews />
+            </Suspense>
+        </div>
+    );
 };
 
 export default ProductReviews;
