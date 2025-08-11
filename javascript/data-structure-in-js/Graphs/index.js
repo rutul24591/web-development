da5c09414d1595a98dcc0f610d5941d55d4cb8af
Rutul Amin <rutulamin.ra@gmail.com>
2025-08-11 13:12:13 +0530

    Adding graphs implementations

---
+++ javascript/data-structure-in-js/Graphs/index.js
@@ -0,0 +1,64 @@
+class Graph {
+    constructor() {
+        this.adjacencyList = {};
+    }
+
+    printGraph() {
+        if (Object.keys(this.adjacencyList).length !== 0) {
+            console.log("{");
+            for (const [key, value] of Object.entries(this.adjacencyList)) {
+                console.log(" ", `${key} : ${value}`);
+            }
+            console.log("}");
+        } else {
+            console.log("{}");
+        }
+    }
+
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) {
+            this.adjacencyList[vertex] = [];
+            return true;
+        }
+
+        return false;
+    }
+
+    addEdge(vertex1, vertex2) {
+        if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+            this.adjacencyList[vertex1].push(vertex2);
+            this.adjacencyList[vertex2].push(vertex1);
+            return true;
+        }
+
+        return false;
+    }
+
+    removeEdge(vertex1, vertex2) {
+        if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+            this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+                (vertex) => vertex !== vertex2
+            );
+            this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+                (vertex) => vertex !== vertex1
+            );
+
+            return true;
+        }
+
+        return false;
+    }
+
+    removeVertex(vertex) {
+        if (!this.adjacencyList[vertex]) return undefined;
+
+        while (this.adjacencyList[vertex].length) {
+            let temp = this.adjacencyList[vertex].pop();
+            this.removeEdge(vertex, temp);
+        }
+
+        delete this.adjacencyList[vertex];
+
+        return this;
+    }
+}
