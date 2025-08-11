d61497556957c738e675e72d3fe0ad16348c3ea7
Rutul Amin <rutulamin.ra@gmail.com>
2025-08-11 13:52:28 +0530

    promises

--- javascript/code/promises/index.js
+++ javascript/code/promises/index.js
@@ -5,7 +5,6 @@
 
 // const cart = ["shoes", "Boxers", "Shirts", "Trousers"];
 
-
 // createOrder(cart);  // orderId;
 
 // proceedToPayment(orderId);
@@ -14,9 +13,9 @@
 
 // Previously we pass callback function.
 // PROBLEM: Passing this way, it becomes responsibility of createOrder to make a call to proceedToPayment API once orderId is recieved.
-// Doing this way it leads to inversion of control. i.e we done have control over createOrder. 
-// Can't blindly trust, might be get called twice or do not call at all, 
-// might be written by some other team or other team member who is junior and might have done proper error handling. 
+// Doing this way it leads to inversion of control. i.e we don't have control over createOrder.
+// Can't blindly trust, might be get called twice or do not call at all,
+// might be written by some other team or other team member who is junior and might have not done proper error handling.
 // Its not reliable.
 
 // createOrder(cart, function(orderId){
@@ -27,8 +26,8 @@
 
 // const promise = createOrder(cart);
 
-//  When JS Engine executes the above line, it will return an empty object(Promise){ data: undefined}, continue with exectuing rest of code and after few seconds 
-// or whatever time it will return the actual data. {data: undefined becomes [{...},{...}]}. Once this is done it will call below automatically
+//  When JS Engine executes the above line, it will return an empty object(Promise){ data: undefined}, continue with exectuing rest of code and after few seconds
+// or whatever time it will return the actual data. {data: undefined }becomes [{...},{...}]}. Once this is done it will call below automatically
 
 // promise.then(function(orderId){
 //     proceedToPayment(orderId);
@@ -36,7 +35,7 @@
 
 // Previously using callbacks, we were passing callback function to another function        <--- !!! Important
 // but here we are attaching the callback function to the promise object.
-// By attaching the callback we have the control of the program with us only. 
+// By attaching the callback we have the control of the program with us only.
 // By doing this way, it guarentees that provided callback function will get called and only once.
 
 // A Promise is an object.           <--- !!!
@@ -46,8 +45,8 @@ const GITHUB_API = "https://api.github.com/users/rutul24591";
 
 const users = fetch(GITHUB_API);
 
-console.log(`users:`, users);      // This log will show Promise: Pending and it you expand it will show Fulfilled (Just a bug in chrome.), Why? 
-// Javascript won't wait  and execute this line before response is received. 
+console.log(`users:`, users); // This log will show Promise: Pending and it you expand it will show Fulfilled (Just a bug in chrome.), Why?
+// Javascript won't wait  and execute this line before response is received.
 
 /**
  * 
@@ -66,26 +65,22 @@ console.log(`users:`, users);      // This log will show Promise: Pending and it
 // 3 stated of promises: Pending , Fulfilled , Rejected
 // The data returned by the promise is immutable, so changes can take place like users[0].name = 'Padm Amin'
 
-users.then(function(data){
+users.then(function (data) {
     console.log(`data: `, data);
 });
 
-
 // INTERVIEWS:
-/** 
- * 1. What is promise? (How to answer) 
- * 
+/**
+ * 1. What is promise? (How to answer)
+ *
  *  Def a. Promise is a placeholder whose value will be filled after some time once the asynchronous operation is finished.
  *  Def b. Promise is a container for future value.
  *  Def c. A Promise is an object representing the eventual completion or failure of an asynchronous operation. ( <--- mdn docs definition)
- * 
+ *
  * */
-
-
 
 /** Above we solve inversion of control issue
  * How to resolve call back hell */
-
 
 // createOrder(cart, function(orderId){
 //     proceedToPayment(orderId, function(paymentInfo){
@@ -97,8 +92,8 @@ users.then(function(data){
 
 // The above code can be considered a callback hell.(Pyramid of Doom). This can be solved using Promise Chaining
 
-// When chaning(using lot ot thens), we want to pipe the data i.e data should flow in the whole chain, 
-// so we pass the data using return 
+// When chaning(using lot ot thens), we want to pipe the data i.e data should flow in the whole chain,
+// so we pass the data using return
 // createOrder(cart)
 //     .then(function(orderId){
 //         return proceedToPayment(orderId);
@@ -116,14 +111,7 @@ users.then(function(data){
 //     .then(paymentInfo => showOrderSummary(paymentInfo))
 //     .then(paymentInfo => updateWalletBalance(paymentInfo));
 
-
 // 2. What are the advantages of promises?
-
-
-
-
-
-
 
 /** PROMISE CHAIN */
 
@@ -141,50 +129,48 @@ const cart = ["shoes", "Boxers", "Shirts", "Trousers"];
 //     console.log(`ERROR ENCOUNTERED`, error.message);
 // });
 
-
 // Lets write it in more clearer fashion
 // Consumer Part
 createOrder(cart)
-    .then(function(orderId){
+    .then(function (orderId) {
         console.log(`orderId:`, orderId);
-        return orderId;    // We can return any data here or a Promise
+        return orderId; // We can return any data here or a Promise
     })
-    // Catch could be placed here as well to handle errors above and continue the chain 
+    // Catch could be placed here as well to handle errors above and continue the chain
     // i.e if cart fails and if you want payment and rest of chain to continue
-    .catch(function(error){
+    .catch(function (error) {
         console.log(`ERROR ENCOUNTERED in createOrder`, error.message);
     })
-    .then(function(orderId) {
+    .then(function (orderId) {
         return proceedToPayment(orderId); // ProceedToPayment is a Promise
     })
-    .then(function(paymentInfo){
-        console.log('paymentInfo', paymentInfo);
+    .then(function (paymentInfo) {
+        console.log("paymentInfo", paymentInfo);
     })
     // Gracefully handle the error, otherwise console will show this error in red.
-    .catch(function(error){
+    .catch(function (error) {
         console.log(`ERROR ENCOUNTERED`, error.message);
-    })
-
+    });
 
 // Let us write the createOrder function (Producer part)
-function createOrder(cart){
+function createOrder(cart) {
     // Create a promise using the Promise constructor and invoke using new keyword.
     // This Promise constructor will take a function which has 2 params: resolve, reject (Provided by javascript by design of Promise API)
-    const prms = new Promise(function(resolve, reject){
+    const prms = new Promise(function (resolve, reject) {
         // createOrder
         // 1. Validate Cart
 
-        if(!validateCart(cart)){
-            // Need to handle it gracefully. Use Catch 
-            const error = new Error("Cart is not valid")
+        if (!validateCart(cart)) {
+            // Need to handle it gracefully. Use Catch
+            const error = new Error("Cart is not valid");
             reject(error);
         }
         // 2. Logic for createOrder.Place order. Interact with database, etc. and receive orderId
         const orderId = "12345";
 
-        if(orderId){
+        if (orderId) {
             // resolve(orderId);
-            setTimeout(function(){
+            setTimeout(function () {
                 resolve(orderId);
             }, 5000);
         }
@@ -193,14 +179,13 @@ function createOrder(cart){
     return prms;
 }
 
-// Dummy 
-function validateCart(cart){
+// Dummy
+function validateCart(cart) {
     return true; // Make it false to reject the promise
 }
 
-
-function proceedToPayment(orderId){
-    return new Promise(function(resolve, reject){
+function proceedToPayment(orderId) {
+    return new Promise(function (resolve, reject) {
         // Do write necessary code for payment
         resolve("Payment Successful");
     });
