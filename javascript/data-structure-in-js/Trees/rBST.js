33e8def4314eedd52a0c807951d53b098a85e7f9
Rutul Amin <rutulamin.ra@gmail.com>
2025-08-11 14:55:04 +0530

    Implemented recusive BST

---
+++ javascript/data-structure-in-js/Trees/rBST.js
@@ -0,0 +1,87 @@
+import Node from "./Node";
+
+class RecusiveBST {
+    constructor() {
+        this.root = null;
+    }
+
+    //Helper function
+    minValue(currentNode) {
+        while (currentNode.left !== null) {
+            currentNode = currentNode.left;
+        }
+        // Return the value of min node and not the pointer to current node
+        return currentNode.value;
+    }
+
+    rContains(value, currentNode = this.root) {
+        if (currentNode === null) return false;
+
+        if (value === currentNode.value) return true;
+
+        if (value < currentNode.value) {
+            return this.rContains(value, currentNode.left);
+        } else {
+            return this.rContains(value, currentNode.right);
+        }
+    }
+
+    // This method is private (#)
+    #rInsert(value, currentNode = this.root) {
+        if (currentNode === null) return new Node(value);
+
+        if (value < currentNode.value) {
+            currentNode.left = this.#rInsert(value, currentNode.left);
+        } else if (value > currentNode.value) {
+            currentNode.right = this.#rInsert(value, currentNode.right);
+        }
+        return currentNode;
+    }
+
+    rInsert(value) {
+        // this will take care when root is null and not the null check in #rInsert(it is meant if we have reached leaf node and check for empty space)
+        if (this.root === null) this.root = new Node(value);
+        this.#rInsert(value);
+    }
+
+    #deleteNode(value, currentNode) {
+        if (currentNode === null) return null;
+
+        if (value < currentNode.value) {
+            currentNode.left = this.#deleteNode(value, currentNode.left);
+        } else if (value > currentNode.value) {
+            currentNode.right = this.#deleteNode(value, currentNode.right);
+        } else {
+            // Four cases here for the node to be deleted
+
+            // 1. It is a leaf node with no children
+            if (currentNode.left === null && currentNode.right === null) {
+                return null;
+            }
+
+            // 2. Leaf node has right child and empty left child
+            else if (currentNode.left === null) {
+                currentNode = currentNode.right;
+            }
+            // 3. Leaf node has left child and empty right child
+            else if (currentNode.right === null) {
+                currentNode = currentNode.left;
+            }
+            // 4. Leaf node has both left and right children.
+            else {
+                let subMinTree = this.minValue(currentNode.right); // Remember minValue returns the value and not pointer
+                currentNode.value = subMinTree;
+                currentNode.right = this.#deleteNode(
+                    subMinTree,
+                    currentNode.right
+                );
+            }
+        }
+
+        return currentNode;
+    }
+
+    deleteNode(value) {
+        this.root = this.#deleteNode(value, this.root);
+    }
+}
