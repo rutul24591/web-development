9b694de56746b66ba6404347b55c1e313b1cd8a0
Rutul Amin <rutulamin.ra@gmail.com>
2025-08-14 16:37:03 +0530

    Adding throttling example

--- javascript/code/throttling/index.js
+++ javascript/code/throttling/index.js
@@ -1,6 +1,49 @@
 // Throttling in Javascript
 // Used for performance optimization
 
-// Suppose if there is a button with an onClick event that called a function which fetches API(which might be expensive). 
+// Suppose if there is a button with an onClick event that called a function which fetches API(which might be expensive).
 // Now if user keeps on clicking one after the other, so there will be several API calls made and it will impact the performance.
 // What we can do is rate limiting the clicks(which is done via throttling)
+
+function throttle(func, delay) {
+    let lastExecutedTime = 0;
+    console.log(`lastExecutedTime: ${lastExecutedTime}`);
+
+    return function (...args) {
+        const currentTime = Date.now();
+
+        if (currentTime - lastExecutedTime >= delay) {
+            console.log(
+                `Call: ${currentTime}, lastExecutedTime: ${lastExecutedTime}`
+            );
+            func.apply(this, args);
+            lastExecutedTime = currentTime;
+        }
+    };
+}
+
+// Usage example:
+function handleScroll() {
+    console.log("Scrolling...");
+}
+
+const throttledScrollHandler = throttle(handleScroll, 1200); // Execute at most once every 200ms
+
+window.addEventListener("scroll", throttledScrollHandler);
+
+// React Throttle hook
+
+// const useThrottle = (func, delay) => {
+//   let timeout = null;
+//   return (...args) => {
+//     if (timeout) {
+//       return;
+//     }
+//     func(...args);
+//     timeout = setTimeout(() => {
+//       timeout = null;
+//     }, delay);
+//   };
+// };
+
+// export default useThrottle;
