206e2ccc64ac6f4dd285a1958f8efa60e78b32ac
Rutul Amin <rutulamin.ra@gmail.com>
2025-09-25 04:31:48 +0530

    Top K Frequent elements

--- leetcode/medium/347.TopKFrequentElements/index.js
+++ leetcode/medium/347.TopKFrequentElements/index.js
@@ -1,150 +1,68 @@
+/* 347. Top K Frequent Elements
+
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+Example 1:
+
+    Input: nums = [1,1,1,2,2,3], k = 2
+
+    Output: [1,2]
+
+Example 2:
+
+    Input: nums = [1], k = 1
+
+    Output: [1]
+
+Example 3:
+
+    Input: nums = [1,2,1,2,1,2,3,1,3,2], k = 2
+
+    Output: [1,2]
+
+Constraints:
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+k is in the range [1, the number of unique elements in the array].
+It is guaranteed that the answer is unique.
+ 
+
+Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size. 
+*/
+
 /**
- * TC: O(n)
- * SC: O(n)
- * Idea: Bucket sort
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
  */
 
-function topKFrequent(nums, k) {
-    let freqMap = new Map();
+// Bucket sort approach
 
-    // Step 1: Count frequencies
-    for (let num of nums) {
-        freqMap.set(num, (freqMap.get(num) || 0) + 1);
+var topKFrequent = function (nums, k) {
+  if (k === 0) return [];
+  const map = new Map();
+
+  for (const num of nums) {
+    map.set(num, (map.get(num) || 0) + 1);
+  }
+
+  const bucket = new Array(nums.length + 1).fill(null).map(() => []);
+
+  for (const [num, count] of map.entries()) {
+    bucket[count].push(num);
+  }
+
+  let result = [];
+
+  for (let f = bucket.length - 1; f >= 0 && result.length < k; f--) {
+    if (bucket[f].length > 0) {
+      for (let num of bucket[f]) {
+        result.push(num);
+        if (result.length === k) break;
+      }
     }
+  }
+  return result;
+};
 
-    // Step 2: Create a bucket where index represents frequency
-    let bucket = Array(nums.length + 1).fill(null).map(() => []);
-    
-    for (let [num, freq] of freqMap.entries()) {
-        bucket[freq].push(num);
-    }
-
-    // Step 3: Collect top k elements from the highest frequency bucket
-    let result = [];
-    for (let i = bucket.length - 1; i >= 0 && result.length < k; i--) {
-        if (bucket[i].length > 0) {
-            result.push(...bucket[i]);
-        }
-    }
-
-    return result.slice(0, k);
-}
-
-// Example usage
-console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2)); // Output: [1, 2]
-
-// This problem could be well solved using heap. But we will need to create a custom heap.
-// TC: O(n log k)
-// SC: O(n + k)
-class MinHeap {
-    constructor() {
-        this.heap = [];
-    }
-
-    size() {
-        return this.heap.length;
-    }
-
-    isEmpty() {
-        return this.size() === 0;
-    }
-
-    insert(val) {
-        this.heap.push(val);
-        this._heapifyUp();
-    }
-
-    remove() {
-        if (this.size() === 1) return this.heap.pop();
-        const min = this.heap[0];
-        this.heap[0] = this.heap.pop();
-        this._heapifyDown();
-        return min;
-    }
-
-    _heapifyUp() {
-        let index = this.size() - 1;
-        while (index > 0) {
-            let parentIdx = Math.floor((index - 1) / 2);
-            if (this.heap[parentIdx][1] <= this.heap[index][1]) break;
-            [this.heap[parentIdx], this.heap[index]] = [this.heap[index], this.heap[parentIdx]];
-            index = parentIdx;
-        }
-    }
-
-    _heapifyDown() {
-        let index = 0;
-        while (2 * index + 1 < this.size()) {
-            let left = 2 * index + 1;
-            let right = 2 * index + 2;
-            let smallest = left;
-
-            if (right < this.size() && this.heap[right][1] < this.heap[left][1]) {
-                smallest = right;
-            }
-
-            if (this.heap[index][1] <= this.heap[smallest][1]) break;
-            [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]];
-            index = smallest;
-        }
-    }
-}
-
-function topKFrequent(nums, k) {
-    let freqMap = new Map();
-
-    for (let num of nums) {
-        freqMap.set(num, (freqMap.get(num) || 0) + 1);
-    }
-
-    let minHeap = new MinHeap();
-    
-    for (let [num, freq] of freqMap.entries()) {
-        minHeap.insert([num, freq]);
-        if (minHeap.size() > k) {
-            minHeap.remove();
-        }
-    }
-
-    let result = [];
-    while (!minHeap.isEmpty()) {
-        result.push(minHeap.remove()[0]);
-    }
-
-    return result.reverse();
-}
-
-console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2)); // Output: [1, 2]
-
-
-// With Nodejs 20+, we can solve using priority queue.
-// TC: O(n log k)
-// SC: O(n + k) 
-
-const { PriorityQueue } = require('node:queue');
-
-function topKFrequent(nums, k) {
-    let freqMap = new Map();
-
-    for (let num of nums) {
-        freqMap.set(num, (freqMap.get(num) || 0) + 1);
-    }
-
-    let minHeap = new PriorityQueue({ compare: (a, b) => a[1] - b[1] });
-
-    for (let [num, freq] of freqMap.entries()) {
-        minHeap.enqueue([num, freq]);
-        if (minHeap.size > k) {
-            minHeap.dequeue();
-        }
-    }
-
-    let result = [];
-    while (minHeap.size > 0) {
-        result.push(minHeap.dequeue()[0]);
-    }
-
-    return result.reverse();
-}
-
-console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2)); // Output: [1, 2]
