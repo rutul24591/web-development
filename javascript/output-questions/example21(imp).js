024ff60e346e03f8f41f290b3385f328b8a90a37
Rutul Amin <rutulamin.ra@gmail.com>
2025-09-30 02:51:59 +0530

    Adding 41-50 output questions

--- javascript/output-questions/example21(imp).js
+++ javascript/output-questions/example21(imp).js
@@ -1,9 +1,8 @@
 /**
  * !!!
  * Q21. Chained setTimeout & Promise
- * 
+ *
  * Explanation:
- * 
  * "x", "end" → synchronous.
  * Microtask (Promise) → "z".
  * Macrotasks: first timeout → "y", then second → "w".
@@ -14,17 +13,16 @@ console.log("x");
 setTimeout(() => console.log("y"), 0);
 
 Promise.resolve().then(() => {
-  console.log("z");
-  setTimeout(() => console.log("w"), 0);
+    console.log("z");
+    setTimeout(() => console.log("w"), 0);
 });
 
 console.log("end");
 
-
 /**
- * 
+ *
  * Output:
- * 
+ *
  * x
  * end
  * z
